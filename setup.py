c89568f9c730bbdaa52cdee3118ca739bb5e35b0
BRISSY Axel <axelbrissy0804@gmail.com>
2025-01-09 20:58:26 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='game_engine',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pygame',
+    ],
+    description='Un moteur de jeu en 2D avec Pygame',
+    author='Brissy Axel',
+    author_email='axelbrissy0804@gmail.com',
+    url='https://github.com/oneblack74/game_engine',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
